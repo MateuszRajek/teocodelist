c69b9714f74f71595ffb56ad84338a2d58b319d0
Mateusz Rajek <mateusz_rajek@wp.pl>
2021-06-01 08:36:53 +0100

    Added checking if list is created before rendering, sorting and filtering

--- src/Components/UsersList.js
+++ src/Components/UsersList.js
@@ -5,8 +5,8 @@ function UsersList({ usersList, searchText }) {
   const [checked, setChecked] = useState(false)
   const [itemsSelected, setItemsSelected] = useState([]);
 
-  const usersSortedAlphabetically = usersList.sort((a, b) => a.last_name > b.last_name ? 1 : -1);
-  const searchedUsers = usersSortedAlphabetically.filter(user => user.first_name.toLowerCase().includes(searchText) || user.last_name.toLowerCase().includes(searchText))
+  const usersSortedAlphabetically = usersList && usersList.sort((a, b) => a.last_name > b.last_name ? 1 : -1);
+  const searchedUsers = usersList && usersSortedAlphabetically.filter(user => user.first_name.toLowerCase().includes(searchText) || user.last_name.toLowerCase().includes(searchText))
 
   //wyświetla listę zanaczonych userów
   console.log(itemsSelected)
@@ -14,13 +14,13 @@ function UsersList({ usersList, searchText }) {
 
   return (
     <>
-    {!searchText && usersList.map(user => {
+    {!searchText && usersList && usersList.map(user => {
       const {first_name, last_name, avatar, id} = user
       return (
         <User {...{ key:id, firstName:first_name, lastName:last_name, avatar, id, checked, setChecked, itemsSelected, setItemsSelected }} />
       )
     })}
-    {searchText && searchedUsers.map(user => {
+    {searchText && usersList && searchedUsers.map(user => {
       const {first_name, last_name, avatar, id} = user
       return (
         <User {...{ key:id, firstName:first_name, lastName:last_name, avatar, id, checked, setChecked, itemsSelected, setItemsSelected }} />
